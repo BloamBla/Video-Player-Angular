49e2582a80603d0ef09e9d5567c1edf354747c4d
stanislau.khramazhankou <stanislau.khramazhankou@specific-group.com>
2018-10-24 11:29:45 +0300

    starting project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
+import { PlayListComponent } from './play-list/play-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    PlayListComponent
   ],
   imports: [
     BrowserModule,
