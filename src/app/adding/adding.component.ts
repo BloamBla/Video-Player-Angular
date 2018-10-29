a422db497cdff693ca6bb4912f8aef24a9cd5cad
stanislau.khramazhankou <stanislau.khramazhankou@specific-group.com>
2018-10-29 16:38:52 +0300

    files

---
+++ src/app/adding/adding.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-adding',
+  templateUrl: './adding.component.html',
+  styleUrls: ['./adding.component.scss']
+})
+export class AddingComponent implements OnInit {
+
+  form = new FormGroup({
+    title: new FormControl('', Validators.required),
+    director: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    preview: new FormControl('', Validators.required),
+    video: new FormControl('', Validators.required)
+  });
+
+  movies: Object = [];
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.movies = this.data.getMovies();
+  }
+
+  addMovie() {
+    this.movies = [...this.movies, this.form.value];
+    this.form.setValue({
+      title: '',
+      director: '',
+      description: '',
+      preview: '',
+      video: ''
+    });
+    this.data.setMovies(this.movies);
+  }
+}
